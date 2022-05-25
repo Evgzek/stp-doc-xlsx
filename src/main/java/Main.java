ebbe488f2d978b406b7dea432860409defbfb5c9
Evgzek <zekamusijhcuk@gmail.com>
2022-05-25 14:56:12 +1000

    add: create sheet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.FileOutputStream;
+
 public class Main {
     public static void main(String[] args) {
+        Workbook wb = new HSSFWorkbook();
+        Sheet sheet = wb.createSheet("test");
 
+        try {
+            FileOutputStream out = new FileOutputStream("test.xls");
+            wb.write(out);
+            out.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
